c86ebc8a2e60e44c804501d0ba0a6899ed63d239
Bernhard <b.korn@boservice.de>
2020-11-20 13:13:04 +0100

    ConsoleApp added(Kennwort1 und Kennwort2)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ***REMOVED*** = "***REMOVED***";
+            string ***REMOVED*** = Deliver***REMOVED***();
+
+            Console.WriteLine($"***REMOVED***: '{***REMOVED***}'");
+            Console.WriteLine($"***REMOVED***: '{***REMOVED***}'");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+        private static string Deliver***REMOVED***()
+        {
+            return "***REMOVED***";
+        }
+    }
+}
